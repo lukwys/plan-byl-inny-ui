1e4351e3cb5697faffefc6c6e857757d0615c104
lukwys <lukasz@ukaszs-MacBook-Pro.local>
2025-05-03 21:52:46 +0200

    about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import { AboutMe } from "@/components/about-me";
+import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch('http://localhost:1337/api/homepage?populate=baner')
+  const { data: homepage } = await data.json()
+
   return (
     <div>
-      <AboutMe />
+      <div className='relative w-full h-[350]'>
+        <Image
+          src={`http://127.0.0.1:1337${homepage.baner.url}`}
+          alt="Zdjęcie o mnie"
+          fill
+        />
+      </div>
+      <div className='grid grid-cols-12 py-10'>
+        <div className='col-start-2 col-span-6'>Content</div>
+        <div className='col-start-9 col-span-3'>
+          <AboutMe />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -1,5 +1,22 @@
-export const AboutMe = () => {
+import Image from 'next/image'
+import Link from 'next/link'
+
+export const AboutMe = async () => {
+  const data = await fetch('http://localhost:1337/api/about-me?populate=avatar')
+  const { data: aboutMe } = await data.json()
+
   return (
-    <div>About me</div>
+    <div className='flex flex-col items-center'>
+      <div className='relative w-[150] h-[150] mb-12'>
+        <Image
+          src={`http://127.0.0.1:1337${aboutMe.avatar.url}`}
+          alt="Zdjęcie o mnie"
+          fill
+        />
+      </div>
+      <p className='font-dm-sans font-semibold mb-2.5'>{aboutMe.title}</p>
+      <p className='text-center font-eb-garamond mb-2.5'>{aboutMe.bio}</p>
+      <Link href='/about' className='text-main-red'>dowiedz się więcej →</Link>
+    </div>
   )
 }
