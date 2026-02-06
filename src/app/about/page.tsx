3153db9487507541e4628c3a3ea2bb0c7049daf8
Lukasz Wysocki <lukasz@Lukaszs-MacBook-Pro.local>
2026-02-06 18:01:04 +0100

    about me

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -23,22 +23,23 @@ const AboutMe = async () => {
           fill
         />
       </div>
-      <div className="grid grid-cols-12 py-10 max-w-7xl mx-auto">
-        <div className="col-start-2 col-span-5">
-          <p className="font-dm-sans font-semibold mb-2.5">{title}</p>
-          <p className="font-eb-garamond mb-2.5">{bio}</p>
-        </div>
-        <div className="col-start-8 col-span-3">
-          <div className="relative w-[500px] h-[500px] mb-12">
+      <div className="grid gap-10 py-10 max-w-7xl mx-auto lg:grid-cols-12">
+        <div className="flex justify-center lg:col-start-8 lg:col-span-3">
+          <div className="relative w-[300px] h-[300px] lg:w-[500px] lg:h-[500px] mb-12">
             <Image
               src={`${STRAPI_URL}${avatar.url}`}
               alt="Zdjęcie o mnie"
               className="object-cover object-center"
-              priority
-              sizes="500px"
+              sizes="(max-width: 1024px) 300px, 500px"
               fill
+              priority
             />
           </div>
+        </div>
+
+        <div className="px-4 lg:px-0 lg:col-start-2 lg:col-span-5">
+          <p className="font-dm-sans font-semibold mb-2.5">{title}</p>
+          <p className="font-eb-garamond mb-2.5">{bio}</p>
         </div>
       </div>
     </div>
