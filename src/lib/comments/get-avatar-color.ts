991671a582be61f40cd449c51d4ed3dfcc14b828
Lukasz Wysocki <lukasz@Lukaszs-MacBook-Pro.local>
2026-02-21 18:54:55 +0100

    avatar

---
+++ src/lib/comments/get-avatar-color.ts
@@ -0,0 +1,10 @@
+export const getAvatarColor = (value: string) => {
+  let hash = 0;
+
+  for (let i = 0; i < value.length; i++)
+    hash = value.charCodeAt(i) + ((hash << 5) - hash);
+
+  const hue = Math.abs(hash) % 360;
+
+  return `hsl(${hue} 60% 45%)`;
+};
